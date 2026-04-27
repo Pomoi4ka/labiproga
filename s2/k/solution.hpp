fb6dd9b8a2eecc7d14b3dca43f489849446b49bf
M0x1m <zhmyax01@gmail.com>
2026-04-27 12:17:48 +0300

    Pull apart solution from main

---
+++ s2/k/solution.hpp
@@ -0,0 +1,24 @@
+#ifndef SOLUTION_HPP_
+#define SOLUTION_HPP_
+
+struct Solution {
+    typedef List<Denom> Payout;
+    List<Payout> solution;
+
+    Solution(File const &file);
+private:
+    typedef List<Denom> Stock;
+    struct Branch { Payout payout; Stock stock; };
+
+    typedef List<Branch> Branches;
+    typedef List<size_t> Agents;
+    typedef Agents::ConstNode AgentNode;
+
+    static size_t count(Payout const&);
+    static Branches generatePayouts(long amount, Stock stock);
+    size_t solve(Stock stock, AgentNode agents, List<Payout> &result);
+};
+
+typedef List<Solution::Payout>::ConstNode FinalPayout;
+
+#endif // SOLUTION_HPP_
